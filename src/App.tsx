7d8038aa877400ac53d76ec358839761894bbdaf
divinityMaker <truedmaker@gmail.com>
2022-09-21 17:40:08 -0300

    feat: add useEffect (endgame, moves)

--- src/App.tsx
+++ src/App.tsx
@@ -70,6 +70,41 @@ const App: React.FC = () => {
     return () => clearInterval(timer);
   }, [playing, timeElapsed]);
 
+  useEffect(() => {
+    if (shownCount === 2) {
+      let opened = gridItems.filter((item) => item.shown === true);
+      if (opened.length === 2) {
+        if (opened[0].item === opened[1].item) {
+          let tmpGrid = [...gridItems];
+          for (let i in tmpGrid) {
+            if (tmpGrid[i].shown) {
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid);
+          setShownCount(0);
+        } else {
+          setTimeout(() => {
+            let tmpGrid = [...gridItems];
+            for (let i in tmpGrid) {
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+          }, 1000);
+        }
+        setMoveCount((state) => state + 1);
+      }
+    }
+  }, [shownCount, gridItems]);
+
+  useEffect(() => {
+    if (moveCount > 0 && gridItems.every((item) => item.permanentShown)) {
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems]);
+
   return (
     <C.Container>
       <C.Info>
@@ -78,7 +113,7 @@ const App: React.FC = () => {
         </C.LogoLink>
         <C.InfoArea>
           <InfoItem label="Tempo" value={formatTimeElapsed(timeElapsed)} />
-          <InfoItem label="Movimentos" value="0" />
+          <InfoItem label="Movimentos" value={moveCount.toString()} />
         </C.InfoArea>
 
         <Button
